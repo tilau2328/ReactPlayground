e3986328514b9445bd31fe0dda8663d013879ddc
André Stielau <a.m.stielau@gmail.com>
2016-11-21 17:34:41 +0000

    gettin into react stuff

---
+++ server.js
@@ -0,0 +1,30 @@
+import { Server } from 'hapi';
+import Inert from 'inert';
+
+const server = new Server();
+server.connection({
+    host: 'localhost',
+    port: 8080
+});
+
+let staticRoute = {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'src/client/public',
+            index: ['index.html']
+        }
+    }
+}
+
+let plugins = [ Inert ];
+let routes = [ staticRoute ];
+
+server.register( plugins,
+    error => {
+        server.route(routes);
+        if (error) return console.error(error);
+        server.start(() => console.log('Server running at:', server.info.uri));
+    }
+);
